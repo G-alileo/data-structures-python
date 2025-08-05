8cb88820d00766cb47d4c4e29d00b206691e1be0
G-alileo <jamespmurithi@gmail.com>
2025-08-05 08:21:20 +0000

    Feat : Addition of Lists data structure

---
+++ list.py
@@ -0,0 +1,12 @@
+# Lists and Their operarions
+
+my_list = ["Apples", "Bananas", "oranges"]
+
+print(my_list)
+
+# List operarions
+# Append
+
+my_list.append("Berries")
+
+print("Appending To a list :", my_list)
